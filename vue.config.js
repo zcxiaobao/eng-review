d74770d5b480c2357f39e6acfdeb3573b731fb0a
zcxiaobao <444186976@qq.com>
2021-10-16 20:55:36 +0800

    微调rem

--- vue.config.js
+++ vue.config.js
@@ -23,8 +23,8 @@ module.exports = {
           require("postcss-pxtorem")({
             rootValue: 37.5, // 换算的基数(设计图750的根字体为32)
             // selectorBlackList: [], // 要忽略的选择器并保留为px。
-            propList: ["*"] //可以从px更改为rem的属性。
-            // minPixelValue: 2 // 设置要替换的最小像素值。
+            propList: ["*"], //可以从px更改为rem的属性。
+            minPixelValue: 2 // 设置要替换的最小像素值。
           })
         ]
       }
